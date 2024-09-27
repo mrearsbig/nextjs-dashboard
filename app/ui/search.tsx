39c664a3817c7d0184cc86d7ed227cd05afca2ef
Alexander Viafara <mrearsbig@gmail.com>
2024-09-26 19:27:09 -0500

    Adding pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,8 +11,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-    
+
     const params = new URLSearchParams(searchParams);
+    
+    params.set('page', '1');
 
     if (term) {
       params.set("query", term);
